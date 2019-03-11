a78ce109549abd1cabd81c9ad36fc7ef2fefb6dd
oHoangNgocThai <hoang.ngoc.thai@framgia.com>
2019-03-11 16:46:22 +0700

    scan bluetooth

--- app/src/main/java/android/thaihn/bluetoothblesample/MainActivity.kt
+++ app/src/main/java/android/thaihn/bluetoothblesample/MainActivity.kt
@@ -1,12 +1,25 @@
 package android.thaihn.bluetoothblesample
 
-import android.support.v7.app.AppCompatActivity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.le.BluetoothLeScanner
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.thaihn.bluetoothblesample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mainBinding: ActivityMainBinding
+
+    private val bluetoothAdapter: BluetoothAdapter? = null
+    private val bleScaner: BluetoothLeScanner? = null
+    private val bleGatt: BluetoothGatt? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
     }
 }
